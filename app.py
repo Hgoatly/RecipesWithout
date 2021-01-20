17ec3197a6d45c3790b965a304c66d47250d4988
Hgoatly <hgoatly@hotmail.com>
2021-01-20 09:46:38 +0000

    Give navbar links correct names.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 
 from flask_pymongo import PyMongo
@@ -19,8 +19,8 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
+@app.route("/home")
+def home():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
