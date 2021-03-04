3a1ce289c56668b3b52777a41da732a10fec80f2
Hgoatly <hgoatly@hotmail.com>
2021-03-04 23:38:28 +0000

    Add 500 error page, remove card from text, and make sure text stays in the centre of the image.

--- app.py
+++ app.py
@@ -502,11 +502,14 @@ def add_recipes():
 
 
 # Error handlers:
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def something_wrong(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
